ed397cb8df0d41df382a250e7b76dbb6ca9d0594
Mary <gmary95@mail.ru>
2018-11-27 10:08:01 +0200

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by GM on 11/27/18.
+//  Copyright © 2018 GM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
